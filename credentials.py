5110a6ad9cacbb9d147a2994f99e2e55ee8bb563
anuj <mesubedianuj@gmail.com>
2017-10-09 10:28:53 +0545

    Git credentials

--- credentials.py
+++ credentials.py
@@ -1,4 +1,11 @@
+import tweepy
+ 
 consumer_key = "your consumer key"
 consumer_secret = "your secret key"
 access_token = "your access token"
 access_token_secret = "your access token secret"
+
+class Twitter:
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	api = tweepy.API(auth)
